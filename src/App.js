636c3c7dbf1973ec058e18d499522a8611bf0437
yeonjin <papio0520@gmail.com>
2024-05-08 19:59:17 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const Homework = () => {
+  const [inputText, setInputText] = useState("");
+  const [comments, setComments] = useState([
+    "안녕하세요",
+    "교육세션 끝",
+    "리액트 재밌으신가요?",
+  ]);
+
+  const handleNewComment = () => {
+    ///이 부분을 작성해주세요
+    //아무 입력도 없을 때에는 댓글이 작성되어선 안됩니다!
+    if (inputText.trim() !== "") { // 입력된 텍스트가 빈 문자열이 아닐 경우에만 실행
+      setComments([...comments, inputText]); // 기존 댓글 목록에 새 댓글 추가
+      setInputText(""); // 입력란을 비움
+    }
+  };
+
+  const handleDeleteComment = (index) => {
+    ///이 부분을 작성해주세요 
+    setComments(comments.filter((_, idx) => idx !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img
+          alt="logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/74/%EB%A9%8B%EC%9F%81%EC%9D%B4%EC%82%AC%EC%9E%90%EC%B2%98%EB%9F%BC_%EB%A1%9C%EA%B3%A0.png"
+          height="64"
+        />
+        <p className="title">멋쟁이 사자처럼 교육세션</p>
       </header>
-    </div>
+      <div className="body">
+        <div className="publish">
+          <input
+            type="text"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+          <button onClick={handleNewComment}>등록</button>
+        </div>
+        {comments.map((item, idx) => (
+          <div key={idx} className="comments">
+            <p>{item}</p>
+            <button onClick={() => handleDeleteComment(idx)}>삭제</button>
+          </div>
+        ))}
+      </div>
+    </>
   );
-}
+};
 
-export default App;
+export default Homework;
+
